d249d129124c055985835a8d3d1e0b095d16df43
Simon Kirkby <tigger@interthingy.com>
2017-05-29 08:44:32 +0800

    base word

--- fth/word.go
+++ fth/word.go
@@ -1,7 +1,24 @@
 package fth
 
+import (
+	"fmt"
+)
+
 // Base interface
 
 type Word interface {
 	Do() (e error)
+}
+
+type BaseWord struct {
+	name  string
+	code  string
+	words []Word
+	count int
+	exec  *func()
+}
+
+func (b *BaseWord) Do() (e error) {
+	fmt.Println("NOTHING HERE ", b.name)
+	return e
 }
