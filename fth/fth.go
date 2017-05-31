341c1b158948f74700eef7dc6f687292925d0949
Simon Kirkby <tigger@interthingy.com>
2017-05-31 14:52:11 +0000

    moved run into fth.go

--- fth/fth.go
+++ fth/fth.go
@@ -1,1 +1,35 @@
 package fth
+
+import (
+	"fmt"
+)
+
+func (fm *ForthMachine) Run(exit chan bool) (e error) {
+	fm.Words()
+	fmt.Printf("Starting on %s\n", fm.startword)
+	w, err := fm.d.Search(fm.startword)
+	if err != nil {
+		fmt.Println("boot error :", err)
+		return err
+	}
+	w.Do()
+	err = fm.LoadFile("./base.fth")
+	if err != nil {
+		return err
+	}
+	for {
+		if fm.exit {
+			exit <- true
+			break
+		}
+		select {
+		case line := <-fm.Input:
+			fmt.Println(line)
+			fm.GetLine(line)
+			fm.Process()
+		}
+		fmt.Println("ok")
+	}
+	fmt.Println("EXIT MACHINE")
+	return
+}
--- fth/machine.go
+++ fth/machine.go
@@ -2,7 +2,6 @@ package fth
 
 import (
 	"bufio"
-	"fmt"
 	"io"
 )
 
@@ -60,7 +59,6 @@ func NewForthMachine(o Options) (fm *ForthMachine) {
 		prompt:    ">",
 	}
 	fm.Complete = NewCompleter(fm)
-	// record the tail of the dictionary
 	return fm
 }
 
@@ -70,34 +68,4 @@ func (fm *ForthMachine) Add(w Word) (err error) {
 
 func (fm *ForthMachine) Words() {
 	fm.d.Words()
-}
-
-func (fm *ForthMachine) Run(exit chan bool) (e error) {
-	fm.Words()
-	fmt.Printf("Starting on %s\n", fm.startword)
-	w, err := fm.d.Search(fm.startword)
-	if err != nil {
-		fmt.Println("boot error :", err)
-		return err
-	}
-	w.Do()
-	err = fm.LoadFile("./base.fth")
-	if err != nil {
-		return err
-	}
-	for {
-		if fm.exit {
-			exit <- true
-			break
-		}
-		select {
-		case line := <-fm.Input:
-			fmt.Println(line)
-			fm.GetLine(line)
-			fm.Process()
-		}
-		fmt.Println("ok")
-	}
-	fmt.Println("EXIT MACHINE")
-	return
 }
